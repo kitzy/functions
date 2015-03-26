12d2c980df6fcabe7e9fe51ecc4ba3fb44f9da7a
John Kitzmiller <kitzy@kitzy.org>
2015-03-26 13:22:51 -0600

    added log function

---
+++ log.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify log name
+log="name.log"
+
+# Create log file
+touch /var/log/"${log}"
+
+# Pipe all output to log
+exec 2>&1 > >(tee /var/log/"${log}")
+
+# Log function
+# Call this function the same way you would use echo
+# EXAMPLE: log "Updating something or other"
+
+log()
+{
+NOW="$(date +"%Y-%m-%d-%H-%M")"
+echo "$NOW": "$1"
+}
